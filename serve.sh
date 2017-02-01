1a877686e6588d5c944e9dfca8642f5070178f39
Egor Tensin <Egor.Tensin@gmail.com>
2017-02-01 22:08:12 +0300

    bind to 0.0.0.0 instead of localhost

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bundle exec jekyll serve --watch --force_polling --config _config.yml,_config_dev.yml
+bundle exec jekyll serve --watch --force_polling --host 0.0.0.0 --config _config.yml,_config_dev.yml
